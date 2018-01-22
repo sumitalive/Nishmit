70b271be8dd9f0d86f71753a9de2bf4de58784ce
sumit <sumit@sumit>
2018-01-22 20:31:14 +0530

    Photos added to Loveline

--- js/function.js
+++ js/function.js
@@ -41,7 +41,7 @@ $(document).on('ready', function () {
         $('.vertical_iconmenu li').removeClass('active');
         $(this).find('a').addClass('active');
         $(this).addClass('active');
-
+        
         setTimeout(function () {
             $('.wow').each(function () {
 
@@ -266,16 +266,7 @@ $(document).on('ready', function () {
 
 
 });
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36251023-1']);
-_gaq.push(['_setDomainName', 'jqueryscript.net']);
-_gaq.push(['_trackPageview']);
 
-(function () {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 
 
 
